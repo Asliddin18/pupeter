e760061032f071a565a5aec0fd3c6bfa20fbf60f
simple123w <122342661+Asliddin18@users.noreply.github.com>
2023-05-21 15:06:29 +0500

    12
    12

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
   puppeteer = require("puppeteer");
 }
 
-app.post("/api", async (req, res) => {
+app.get("/api/:pages", async (req, res) => {
   let options = {};
 
   if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
@@ -26,7 +26,7 @@ app.post("/api", async (req, res) => {
     let browser = await puppeteer.launch(options);
 
     let page = await browser.newPage();
-    await page.goto(req.body.pages);
+    await page.goto(req.params.pages);
     await page.waitForSelector("#prodImgdiv1")
     var exp = await page.$eval("#prodImgdiv1" , (el)=>el.innerHTML)
     res.status(200).send(exp)

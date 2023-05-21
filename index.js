73966f304730d38438c6a4d14a803b156862d631
simple123w <122342661+Asliddin18@users.noreply.github.com>
2023-05-21 15:20:38 +0500

    12
    12

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const app = require("express")();
 
+const cheerio = require("cheerio")
+const { default:axios } = require("axios");
 let chrome = {};
 let puppeteer;
+
+
 
 if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
   chrome = require("chrome-aws-lambda");
@@ -35,6 +39,156 @@ app.get("/api", async (req, res) => {
     return null;
   }
 });
+
+
+app.get("/product/:category/:minicategory/:padcategory", async (req, res) => {
+  var url = `https://www.diamondsfactory.com/${req.params.category}/${req.params.minicategory}/${req.params.padcategory}?page=${req.quary.number}`
+  const { data } = await axios({
+      method: 'GET',
+      url: url
+  })
+  var select = "div.middle-container div.categoryDiv2"
+  var a = []
+  const $ = cheerio.load(data)
+  $(select).each((index, item) => {
+      $(item).children().each((index, data) => {
+          var pushdata = JSON.parse($(data).attr().onclick.slice(15, -2))
+                      var d=$(data, "div.showbox").html()
+          var b=d.indexOf('class="sale-label">SALE</div>\n<a')
+   
+
+          var t=d.indexOf('tabindex="1" class="catLink"')
+      
+          var y=d.slice(0,b+33)+d.slice(t)
+          pushdata.code = `${y}`
+          pushdata.img = $(data).find('img').attr("src")
+          a.push(pushdata)
+      }
+      )
+  })
+  
+  res.status(200).send(a)
+
+
+})
+app.get("/product/:category/:minicategory", async (req, res) => {
+  var url = `https://www.diamondsfactory.com/${req.params.category}/${req.params.minicategory}?page=${req.query.number}`
+  const { data } = await axios({
+      method: 'GET',
+      url: url
+  })
+  var select = "div.middle-container div.categoryDiv2"
+  var a = []
+  const $ = cheerio.load(data)
+  $(select).each((index, item) => {
+      $(item).children().each((index, data) => {
+          var pushdata = JSON.parse($(data).attr().onclick.slice(15, -2))
+                      var d=$(data, "div.showbox").html()
+          var b=d.indexOf('class="sale-label">SALE</div>\n<a')
+   
+
+          var t=d.indexOf('tabindex="1" class="catLink"')
+      
+          var y=d.slice(0,b+33)+d.slice(t)
+          pushdata.code = `${y}`
+          pushdata.img = $(data).find('img').attr("src")
+          a.push(pushdata)
+      }
+      )
+  })
+ 
+  res.status(200).send(a)
+
+
+})
+
+app.get("/product/:category", async (req, res) => {
+  var url = `https://www.diamondsfactory.com/${req.params.category}?page=${req.query.number}`
+  const { data } = await axios({
+      method: 'GET',
+      url: url
+  })
+  var select = "div.middle-container div.categoryDiv2"
+  var a = []
+  const $ = cheerio.load(data)
+  $(select).each((index, item) => {
+      $(item).children().each((index, data) => {
+          var pushdata = JSON.parse($(data).attr().onclick.slice(15, -2))
+          var d=$(data, "div.showbox").html()
+          var b=d.indexOf('class="sale-label">SALE</div>\n<a')
+   
+
+          var t=d.indexOf('tabindex="1" class="catLink"')
+      
+          var y=d.slice(0,b+33)+d.slice(t)
+          pushdata.code = `${y}`
+          pushdata.img = $(data).find('img').attr("src")
+          a.push(pushdata)
+      }
+      )
+  })
+ 
+  res.status(200).send(a)
+
+
+})
+app.get('/page/:category/:minicategory', async (req, res) => {
+  var url = `https://www.diamondsfactory.com/${req.params.category}/${req.params.minicategory}`
+  var a = 0
+  const { data } = await axios({
+      method: 'GET',
+      url: url
+  })
+  var select = "div.row.row-compact div.col-sm-12.col-xs-12.text-center div.pagination div.links a.paginationlink"
+
+  const $ = cheerio.load(data)
+  $(select).each((index, item) => {
+    
+      if ($(item).text().length <= 3) {
+          a = $(item).text()
+      }
+  })
+  res.status(200).send(`${a}`)
+})
+
+app.get('/page/:category/:minicategory/:padcategory', async (req, res) => {
+  var url = `https://www.diamondsfactory.com/${req.params.category}/${req.params.minicategory}/${req.params.padcategory}`
+  var a = 0
+  const { data } = await axios({
+      method: 'GET',
+      url: url
+  })
+  var select = "div.row.row-compact div.col-sm-12.col-xs-12.text-center div.pagination div.links a.paginationlink"
+
+  const $ = cheerio.load(data)
+  $(select).each((index, item) => {
+    
+      if ($(item).text().length <= 3) {
+          a = $(item).text()
+      }
+  })
+  res.status(200).send(`${a}`)
+})
+app.get('/page/:category', async (req, res) => {
+  var url = `https://www.diamondsfactory.com/${req.params.category}`
+  var a = 0
+  const { data } = await axios({
+      method: 'GET',
+      url: url
+  })
+  var select = "div.row.row-compact div.col-sm-12.col-xs-12.text-center div.pagination div.links a.paginationlink"
+
+  const $ = cheerio.load(data)
+  $(select).each((index, item) => {
+      
+      if ($(item).text().length <= 3) {
+          a = $(item).text()
+      }
+  })
+  res.status(200).send(`${a}`)
+})
+
+
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server started");
